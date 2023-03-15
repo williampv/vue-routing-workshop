5803e427d0f1d779eb73e208bbdae054b8842ef8
William <williampv@gmail.com>
2023-03-15 09:39:36 -0500

    404 page created

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Foo from './components/Foo.vue';
 import Bar from './components/Bar.vue';
+import ErrorNotFound from './components/404.vue';
 
 const routes = [
 
     { path: '/foo', component: Foo },
-    { path: '/bar', component: Bar }
+    { path: '/bar', component: Bar },
+    { path: '/:pathMatch(.*)*', component: ErrorNotFound }
 
 ]
 
